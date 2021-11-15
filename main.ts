17e53a4b432743dc558e3a7daa2b9419620a7d3a
cdtdundee <76946982+cdtdundee@users.noreply.github.com>
2021-11-15 15:47:25 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,32 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 0) {
         music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+        servos.P2.run(50)
+        servos.P1.run(-52)
     }
+    if (receivedNumber == 1) {
+        music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+        servos.P1.run(50)
+        servos.P2.run(-52)
+    }
+    if (receivedNumber == 2) {
+        music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+        servos.P1.run(24)
+        servos.P2.run(11)
+    }
+    if (receivedNumber == 3) {
+        music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+        servos.P1.run(-13)
+        servos.P2.run(-24)
+    }
+    if (receivedNumber == 4) {
+        music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+        servos.P1.run(0)
+        servos.P2.run(0)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(4)
 })
 input.onGesture(Gesture.LogoUp, function () {
     radio.sendNumber(0)
