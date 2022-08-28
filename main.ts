4bd04953c2dbc47ca958a337fb9c374bd16f6396
cdtdundee <76946982+cdtdundee@users.noreply.github.com>
2022-08-28 11:14:29 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -40,7 +40,27 @@ input.onGesture(Gesture.TiltRight, function () {
 input.onGesture(Gesture.LogoDown, function () {
     radio.sendNumber(1)
 })
+let strip = neopixel.create(DigitalPin.P0, 3, NeoPixelMode.RGB)
 radio.setGroup(1)
 basic.forever(function () {
-    basic.showIcon(IconNames.Ghost)
+    for (let index = 0; index <= 2; index++) {
+        strip.setPixelColor(index, neopixel.colors(NeoPixelColors.Indigo))
+        strip.show()
+        basic.pause(500)
+    }
+    for (let index = 0; index <= 2; index++) {
+        strip.setPixelColor(index, neopixel.colors(NeoPixelColors.Orange))
+        strip.show()
+        basic.pause(500)
+    }
+    for (let index = 0; index <= 2; index++) {
+        strip.setPixelColor(index, neopixel.colors(NeoPixelColors.Purple))
+        strip.show()
+        basic.pause(500)
+    }
+    for (let index = 0; index <= 2; index++) {
+        strip.setPixelColor(index, neopixel.colors(NeoPixelColors.Green))
+        strip.show()
+        basic.pause(500)
+    }
 })
